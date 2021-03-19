a1c8879b8126f9ca6d0902e7ed297cc31907c8a3
Hadad <repo-sync@outlook.com>
2021-03-19 19:21:32 +0700

    Revert "wlan: Introduce TARGET_HAS_BROKEN_WLAN_SET_INTERFACE"
    This reverts commit 3d3c911a318aae021fb44e9c15b4496097ff24f5.
    
    Signed-off-by: Hadad <repo-sync@outlook.com>

--- qcwcn/wifi_hal/Android.mk
+++ qcwcn/wifi_hal/Android.mk
@@ -46,10 +46,6 @@ ifneq ($(TARGET_USES_AOSP_FOR_WLAN), true)
 LOCAL_CFLAGS += -DWCNSS_QTI_AOSP
 endif
 
-ifeq ($(TARGET_HAS_BROKEN_WLAN_SET_INTERFACE),true)
-LOCAL_CFLAGS += -DBROKEN_SET_INTERFACE
-endif
-
 # gscan.cpp: address of array 'cached_results[i].results' will always evaluate to 'true'
 LOCAL_CLANG_CFLAGS := -Wno-pointer-bool-conversion
 
@@ -118,10 +114,6 @@ endif
 
 ifneq ($(TARGET_USES_AOSP_FOR_WLAN), true)
 LOCAL_CFLAGS += -DWCNSS_QTI_AOSP
-endif
-
-ifeq ($(TARGET_HAS_BROKEN_WLAN_SET_INTERFACE),true)
-LOCAL_CFLAGS += -DBROKEN_SET_INTERFACE
 endif
 
 # gscan.cpp: address of array 'cached_results[i].results' will always evaluate to 'true'
